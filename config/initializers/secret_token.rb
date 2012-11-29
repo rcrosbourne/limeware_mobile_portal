244cfe353491ae05f2437c6fc7b48a076ddc153c
R.Crosbourne <rcrosbourne@gmail.com>
2012-11-29 10:31:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LimewareMobilePortal::Application.config.secret_token = '64c7c4cd5e2b8c7ed30206560af8ef7dfa6bbcebf5ca3185c573bb0a64c93514013f4864342feb459f7fa57ad1499ff2326f75f9624c4ef25294baed1421ecdd'
